efff8202739f40e1f6ecc2baa5e95f18f6afd661
SeungHan <jk05018@naver.com>
2022-04-28 18:05:46 +0900

    fix : CORS 에러로 인핸 WebMvcConfiguer 설정 변경

---
+++ src/main/java/org/prgms/gccoffee/controller/api/ProductRestController.java
@@ -0,0 +1,28 @@
+package org.prgms.gccoffee.controller.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.prgms.gccoffee.model.Category;
+import org.prgms.gccoffee.model.Product;
+import org.prgms.gccoffee.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+	private final ProductService productService;
+
+	public ProductRestController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping("/api/v1/products")
+	public List<Product> productList(@RequestParam Optional<Category> category){
+		return category
+			.map(productService::getProductByCategory)
+			.orElse(productService.getAllProducts());
+	}
+}
